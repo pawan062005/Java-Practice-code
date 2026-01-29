fd2e112e81c18ac20fbb61ce8714210d4faaf463
pawan062005 <pawansahni2005@gmail.com>
2026-01-29 16:34:59 +0530

    add some pattern question

---
+++ starpat.java
@@ -0,0 +1,17 @@
+public class starpat {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 3) {
+
+            int j = 0;
+            while (j <= 3) {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+}
---
+++ triangle.java
@@ -0,0 +1,17 @@
+public class triangle {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 3) {
+
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+}
+  
